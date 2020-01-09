6f97e43262db920d6109fa385bf01f5d1b9e8ce8
Ben Keith <ben@benlk.com>
2020-01-09 16:57:01 -0500

    Make the search icon a search icon, and continue work on search styles

--- wp-content/themes/sfpublicpress/partials/nav-global.php
+++ wp-content/themes/sfpublicpress/partials/nav-global.php
@@ -41,7 +41,7 @@ if (
 								<div id="header-search">
 									<form class="form-search" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 										<div class="input-append">
-											<input type="text" placeholder="<?php _e('Search', 'largo'); ?>" class="input-medium appendedInputButton search-query" value="" name="s" /><button type="submit" class="search-submit btn"><?php _e('GO', 'largo'); ?></button>
+											<input type="text" placeholder="<?php _e('Search', 'largo'); ?>" class="input-medium appendedInputButton search-query" value="" name="s" /><button type="submit" class="search-submit btn" aria-label="<?php esc_attr_e( 'Submit search', 'sfpublicpress' ); ?>"><span class="dashicons dashicons-search"></span></button>
 										</div>
 									</form>
 								</div>
